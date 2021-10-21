f3388c7167032547a3e1a5b7a5c475c04fe8e120
Sam Warnick <sam.t.warnick@gmail.com>
2021-10-20 20:01:21 -0400

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as log from "https://deno.land/std@0.107.0/log/mod.ts";
-export { readLines } from "https://deno.land/std@0.107.0/io/mod.ts";
-export * as path from "https://deno.land/std@0.107.0/path/mod.ts";
+export * as log from "https://deno.land/std@0.112.0/log/mod.ts";
+export { readLines } from "https://deno.land/std@0.112.0/io/mod.ts";
+export * as path from "https://deno.land/std@0.112.0/path/mod.ts";
